072c34fffff37c621cceef7dfa1396a256c4d158
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-06-21 09:18:11 +0300

    update serve to nwe twistd parameters

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo /usr/bin/twistd -n web --path . -p 80
+#sudo /usr/bin/twistd -n web --path . -p 80
+#sudo /usr/bin/twistd -n web --path . --port=80
+sudo /usr/bin/twistd -n web --path . --port=tcp:80
